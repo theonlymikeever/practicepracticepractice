827e9cad76341dbb2ac082d3ec86a9128a6e8a7e
Mike Kerslake <theonlymikeever@gmail.com>
2018-12-05 10:00:42 -0500

    LC - needleInHaystack

--- leetCode/needInHaystack.js
+++ leetCode/needInHaystack.js
@@ -3,7 +3,7 @@
   Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
   Difficulty:
-    East
+    Easy
 
   URL:
     https://leetcode.com/problems/implement-strstr/
