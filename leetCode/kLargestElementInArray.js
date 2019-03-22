be19098d6392941272393c084036ae51ac0ca308
Mike Kerslake <theonlymikeever@gmail.com>
2019-03-22 16:38:58 -0400

    Improve for edge cases

--- leetCode/kLargestElementInArray.js
+++ leetCode/kLargestElementInArray.js
@@ -21,26 +21,27 @@
 
 // Fast answer
 var findKthLargest = function(nums, k) {
-  const sorted = nums.sort();
-  return sorted[sorted.length - k];
+  const sorted = nums.sort((a,b) => b - a);
+  return sorted[k - 1];
 };
 
 var findKthLargest2 = function(nums, k) {
-  // sort the array (bubble)
+  // reverse sort the array (bubble)
   let swapping;
   do {
     swapping = false;
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] && nums[i + 1] && nums[i] > nums[i + 1]) {
+    for (let i =  nums.length; i > 0; i--) {
+      if (nums[i] && nums[i - 1] && nums[i] > nums[i - 1]) {
         // swap!
         let temp = nums[i];
-        nums[i] = nums[i + 1];
-        nums[i + 1] = temp;
+        nums[i] = nums[i - 1];
+        nums[i - 1] = temp;
         swapping = true;
       }
     }
   } while (swapping);
-  return nums[nums.length - k];
+  console.log(nums)
+  return nums[k - 1];
 };
 
 const tests = [
@@ -53,12 +54,22 @@ const tests = [
     input: [3,2,3,1,2,4,5,5,6],
     k: 4,
     output: 4
+  },
+  {
+    input: [-1,2,0],
+    k: 1,
+    output: 2
+  },
+  {
+    input: [-1,2,0],
+    k: 2,
+    output: 0
   }
 ];
 
 tests.forEach(({input, k, output}) => {
   const fast = findKthLargest(input, k);
   const long = findKthLargest2(input, k);
-  console.log(`Answer with easy solution: ${fast} - ${fast === output ? 'Correct!' : 'WRONG!'}`);
-  console.log(`Answer with easy solution: ${long} - ${long === output ? 'Correct!' : 'WRONG!'}`);
+  console.log(`Answer with fast solution: ${fast} - ${fast === output ? 'Correct!' : 'WRONG!'}`);
+  console.log(`Answer with long solution: ${long} - ${long === output ? 'Correct!' : 'WRONG!'}`);
 });
