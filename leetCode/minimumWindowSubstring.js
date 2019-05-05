4f7ccb4d01f2480571fc7bc6e8963b7031ec358b
Mike Kerslake <theonlymikeever@gmail.com>
2019-05-05 13:57:07 -0400

    LC - minimumWindowSubstring finished

--- leetCode/minimumWindowSubstring.js
+++ leetCode/minimumWindowSubstring.js
@@ -17,13 +17,52 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
+  let window = '';
+  // edge case: empty string or empty sub or shorter string than sub
+  if (s.length < t.length || !s.length || !t.length) return window;
+  // Map of all charters we need to attribute and length of sub
+  let map = {};
+  for (let i = 0; i < t.length; i++) {
+    map[t[i]] = (map[t[i]] || 0) + 1;
+  }
+  let requiredSize = Object.keys(map).length;
 
+  // Left and right pointers for window
+  let p1 = 0;
+  let p2 = -1;
+  // Loop through until right pointer hits end
+  while (p2 < s.length) {
+    if (requiredSize === 0) {
+      // we've met requirements for substring - update window
+      if (!window || p2 - p1 + 1 < window.length) {
+        window = s.slice(p1, p2 + 1);
+      }
+      // remove from map and shift left pointer
+      if (map[s[p1]] !== undefined) {
+        map[s[p1]] += 1;
+      }
+      if (map[s[p1]] > 0) {
+        requiredSize += 1;
+      }
+      p1 += 1;
+    } else {
+      // window does not contain all of t, must move p2
+      p2 += 1;
+      if (map[s[p2]] !== undefined) {
+        map[s[p2]] -= 1;
+      }
+      if (map[s[p2]] === 0) {
+        requiredSize -= 1;
+      }
+    }
+  }
+  return window;
 };
 
 const testInput = {
   s: 'ADOBECODEBANC',
   t: 'ABC'
-}
+};
 const expectedOutput = 'BANC';
 const result = minWindow(testInput.s, testInput.t);
 
