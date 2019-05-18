bfbd22b6cba3cb521647a489a2d374fa14968693
Mike Kerslake <theonlymikeever@gmail.com>
2019-05-18 15:01:52 -0400

    AC - LC: linkedListCycleII finished!

--- leetCode/linkedListCycleII.js
+++ leetCode/linkedListCycleII.js
@@ -41,20 +41,42 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var detectCycle = function(head) {
+  if (head === null || head.next === null) return null;
 
+  let p1 = head; // slow pointer
+  let p2 = head; // fast pointer
+
+  // iterate through and move the fast pointer two at a time
+  // eventually the fast pointer will catch up to the first pointer indicating the 'tail'
+  // once caught, move the head through, as well as the second pointer until they meet again
+  // they will continue to move until p2 is at the end again, and the head will be at the circular reference
+  while (p2 !== null && p2.next !== null) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+    if (p1 === p2) {
+      while (head !== p2) {
+        head = head.next;
+        p2 = p2.next;
+      }
+      return head;
+    }
+  }
+  // if we hit the end and theirs no circular ref, return null
+  return null;
 };
 
 function buildTest(vals, pos) {
   let stack = [];
   let current = new ListNode(vals.shift());
   let head = current;
-  stack.push(head)
-  while (vals.length){
-    current.next = new ListNode(vals.shift());
-    stack.push(current);
+  stack.push(head);
+  while (vals.length) {
+    let val = vals.shift();
+    current.next = new ListNode(val);
     current = current.next;
+    stack.push(current);
   }
-  if (pos) {
+  if (pos > -1) {
     current.next = stack[pos];
   }
   return head;
@@ -62,19 +84,23 @@ function buildTest(vals, pos) {
 
 let tests = [
   {
-    vals: [3,2,0,-4],
-    pos: -1
+    vals: [3, 2, 0, -4],
+    pos: 1
   },
   {
-    vals: [1,2],
+    vals: [1, 2],
     pos: 0
   },
   {
     vals: [1],
     pos: -1
   },
+  {
+    vals: [-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5],
+    pos: -1
+  }
 ];
 
 let inputs = tests.map(({ vals, pos }) => buildTest(vals, pos));
 
-console.log(inputs)
+inputs.forEach(item => console.log(detectCycle(item)));
